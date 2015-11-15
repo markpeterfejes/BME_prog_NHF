ee635f1c100c77862dbb1a8a9b7e6880a904bdea
markfejes <fejes.mark@gmail.com>
2015-11-15 15:33:47 +0100

    Inclusions

--- NHF1/NHF1/main.c
+++ NHF1/NHF1/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
 
 int main(void) {
 
---
+++ NHF1/NHF1/matrix.h
@@ -0,0 +1,11 @@
+#ifndef MATRIX_H_INCLUDED
+#define MATRIX_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+#endif
